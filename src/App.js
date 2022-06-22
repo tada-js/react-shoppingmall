44241b24a803a99c15446477def27f7d564f756a
tada-js <justfix010@gmail.com>
2022-06-22 11:28:59 +0900

    :art: 추가 : 상품 컴포넌트화
    추가 : Card 컴포넌트화, props

--- src/App.js
+++ src/App.js
@@ -27,32 +27,26 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="80%"
-            ></img>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="80%"
-            ></img>
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="80%"
-            ></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      ></img>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
