fb4f80bfd1cee5c4525ca80f8a6ca668ee74471f
tada-js <justfix010@gmail.com>
2022-07-03 22:16:26 +0900

    :sparkles: feat: useEffect() 학습
    Detail 컴포넌트 useEffect 추가

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
+import { isCompositeComponentWithType } from "react-dom/test-utils";
 import { useParams } from "react-router-dom";
 
+
+
 function Detail(props) {
+  
   let {id} = useParams()
+  let [alert, setAlert] = useState(true )
+
+  useEffect(() => {
+    setTimeout(()=>{setAlert(false)}, 2000)
+  })
 
   return (
     <div className="container">
+      {
+        alert === true
+        ? <div className="alert alert-warning">2초이내 구매시 할인</div>
+        : null
+      }
       <div className="row">
         <div className="col-md-6">
           <img
